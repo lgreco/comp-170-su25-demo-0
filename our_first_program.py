2e1fbe85f1d70fb3f4729a2b7c0c2c10e92553b8
Leo Irakliotis <lgreco@gmail.com>
2025-05-20 17:22:16 +0000

    Lots of stuff

--- our_first_program.py
+++ our_first_program.py
@@ -1,1 +1,24 @@
-print("Hello COMP 170!")
+
+print("Hello Kofi, welcome to COMP 271.")
+print("Hello Richard, welcome to COMP 271.")
+print("Hello Lillie, welcome to COMP 170.")
+print("Hello Heather, welcome to COMP 170.")
+print("Hello Ben, wellco to COMp 177")
+
+# Pattern: 
+# "Hello" first_name ", welcome to " course_code
+
+def personalized_greeting(name, course_code):
+    print(f"Hello {name}, welcome to {course_code} !!!")
+
+personalized_greeting("Cesar", "COMP 170")
+personalized_greeting("Lula", "COMP 170")
+
+print()
+
+
+students = ["Omar", "Lula", "Enrique", "Arhub", "Ben", "Richard", "Heather"]
+your_course = "COMP 170"
+
+for name in students:
+    personalized_greeting(name, your_course)
